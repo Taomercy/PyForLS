9d4486510773909ee96a90206ce99baf682ad9a7
Taomercy <taomercy@qq.com>
2020-01-07 16:55:40 +0800

    import des

---
+++ desentisize.py
@@ -0,0 +1,91 @@
+import re
+
+
+def match_telphone(data):
+    p = re.compile(r"^1[3-9]\d{9}$")
+    res = p.findall(data)
+    return res
+
+def match_email(data):
+    p = re.compile(r"\w{0,19}@[0-9a-zA-Z]{1,13}\.[com,cn,net]{1,3}")
+    res = p.findall(data)
+    return res
+
+def match_idcard(data):
+    p = re.compile(r"^[1-6]\d{5}[12]\d{3}(0[1-9]|1[12])(0[1-9]|1[0-9]|2[0-9]|3[01])\d{3}(\d|X|x)$")
+    res = p.findall(data)
+    return res
+
+def match_mobel(data):
+    p = re.compile(r"\d{3}-\d{8}|\d{4}-\d{7}")
+    res = p.findall(data)
+    return res
+
+class Desensitization(object):
+    def __init__(self, email=None, mobile=None, identity_card=None, address=None):
+        self.email = email
+        self.mobile = mobile
+        self.identity_card = identity_card
+        self.address = address
+
+    def get_email_complex(self):
+        # 通过@分开字符串
+        mail_split_list = self.email.split('@')
+        # 前面2位显示
+        dis_mail_pre = mail_split_list[0][:2]
+        # 其它位数隐藏
+        hide_mail_pre = mail_split_list[0][2:]
+        # 最后2位显示
+        dis_mail_last = mail_split_list[-1][-2:]
+        # 其它位数隐藏
+        hide_mail_last = mail_split_list[-1][:-2]
+        # 使用re.sub过滤
+        result_hide_mail_pre = re.sub(hide_mail_pre, len(hide_mail_pre) * '*', hide_mail_pre)
+        # 使用re.sub过滤
+        result_hide_mail_last = re.sub(hide_mail_last, len(hide_mail_last) * '*', hide_mail_last)
+        # 将结果集合起来
+        desensitization_mail = dis_mail_pre + result_hide_mail_pre + '@' + result_hide_mail_last + dis_mail_last
+        return desensitization_mail
+
+    def get_email(self):
+        hide_mail_content = self.email[2:-2]
+        result = re.sub(hide_mail_content, len(hide_mail_content) * '*', self.email)
+        return result
+
+    def get_mobile(self):
+        hide_mail_content = self.mobile[3:-4]
+        result = re.sub(hide_mail_content, len(hide_mail_content) * '*', self.mobile)
+        return result
+
+    def get_identity_card(self):
+        hide_identity_card = self.identity_card[3:-4]
+        result = re.sub(hide_identity_card, len(hide_identity_card) * '*', self.identity_card)
+        return result
+
+    def get_address(self):
+        hide_address_content = self.address[3:-3]
+        result = re.sub(hide_address_content, len(hide_address_content) * '*', self.address)
+        return result
+
+
+if __name__ == "__main__":
+    a = "1228291335@qq.com"
+    b = "17621063414"
+    c = "021-65979152"
+
+    print(match_email(a))
+    print(match_telphone(b))
+    print(match_mobel(c))
+    
+    result_email = Desensitization(email='linqunbin@126.com')
+    print(result_email.get_email())
+
+    result_mobile = Desensitization(mobile='18911112222')
+    print(result_mobile.get_mobile())
+
+    result_id = Desensitization(identity_card='123123200001011234')
+    print(result_id.get_identity_card())
+
+    result_address = Desensitization(address="上海市虹口区某某路某某号123室")
+    print(result_address.get_address())
+
--- desentisize_test.py
+++ desentisize_test.py
@@ -2,6 +2,16 @@ import re
 import threading
 import time
 from threading import Thread
+from desentisize import Desensitization
+
+
+def span_cal(func):
+    def func_wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        print("span:", time.time() - start)
+        return func
+    return func_wrapper
 
 
 class Desentisize(object):
@@ -9,28 +19,34 @@ class Desentisize(object):
 
     def __init__(self, data):
         self.input = data
-        self.rules = [self.match_telphone, self.match_email, self.match_idcard, self.match_mobel]
+        self.rules = [self.match_email, self.match_idcard, self.match_mobile]
         self.output = []
-
-    def match_telphone(self, context):
-        p = re.compile(r"^1[3-9]\d{9}$")
-        res = p.findall(context)
-        return res
 
     def match_email(self, context):
         p = re.compile(r"\w{0,19}@[0-9a-zA-Z]{1,13}\.[com,cn,net]{1,3}")
         res = p.findall(context)
-        return res
+        if res:
+            return Desensitization(email=context).get_email()
+        return None
 
     def match_idcard(self, context):
         p = re.compile(r"^[1-6]\d{5}[12]\d{3}(0[1-9]|1[12])(0[1-9]|1[0-9]|2[0-9]|3[01])\d{3}(\d|X|x)$")
         res = p.findall(context)
-        return res
+        if res:
+            return Desensitization(identity_card=context).get_identity_card()
+        return None
 
-    def match_mobel(self, context):
+    def match_mobile(self, context):
         p = re.compile(r"\d{3}-\d{8}|\d{4}-\d{7}")
         res = p.findall(context)
-        return res
+        if res:
+            return Desensitization(mobile=context).get_mobile()
+        return None
+
+    # def match_address(self, context):
+    #     if res:
+    #         return Desensitization(address=context).get_address()
+    #     return None
 
     def thread_func(self, json, threadLock):
         threadLock.acquire()
@@ -38,12 +54,12 @@ class Desentisize(object):
             for rule in self.rules:
                 res = rule(value)
                 if res:
-                    json[key] = value.replace(res[0], "****")
+                    json[key] = res
         self.output.append(json)
         threadLock.release()
 
+    @span_cal
     def deal(self):
-        start = time.time()
         threads = []
         for info in self.input:
             threadLock = threading.Lock()
@@ -52,12 +68,12 @@ class Desentisize(object):
             threads.append(thread)
         for t in threads:
             t.join()
-        print("span:", time.time() - start)
 
 
-input_data = {'email': "1228291335@qq.com",
-              "mobile-phone": "17621063414",
-              "tel-phone": "021-65979152"}
+input_data = {'email': "linqunbin@126.com",
+              "mobile-phone": "18911112222",
+              "identity_card": "123123200001011234",
+              "address": "上海市虹口区某某路某某号123室"}
 
 json_list = []
 for i in range(0, 500):
